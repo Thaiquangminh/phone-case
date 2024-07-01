94474b60b32e6688b23d2aa5cb3d3eb262394166
Thai Quang Minh <minhtq@notavn.com>
2024-07-01 18:02:18 +0700

    Add drag and drop

--- src/app/configure/upload/page.tsx
+++ src/app/configure/upload/page.tsx
@@ -1,7 +1,55 @@
 "use client";
 
+import { cn } from "@/lib/utils";
+import { Image, Loader2, MousePointerSquareDashed } from "lucide-react";
+import { useState } from "react";
+import DropZone, { FileRejection } from "react-dropzone";
+
 const Page = () => {
-  return <div className="ring-1 ring-inset">Create your case</div>;
+  const [isDragOver, setIsDragOver] = useState(false);
+  const isUploading = false;
+  const onDropRejected = () => {};
+  const onDropAccepted = () => {};
+  return (
+    <div
+      className={cn(
+        "relative h-full flex-1 my-16 w-full rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:rounded-2xl flex justify-center flex-col items-center",
+        {
+          "ring-blue-900/25 bg-blue-900/10": isDragOver,
+        }
+      )}
+    >
+      <div className="relative flex flex-1 flex-col items-center justify-center w-full">
+        <DropZone
+          onDropRejected={onDropRejected}
+          onDropAccepted={onDropAccepted}
+          accept={{
+            "image/png": [".png"],
+            "image/jpeg": [".jpeg"],
+            "image/jpg": [".jpg"],
+          }}
+          onDragEnter={() => setIsDragOver(true)}
+          onDragLeave={() => setIsDragOver(false)}
+        >
+          {({ getInputProps, getRootProps }) => (
+            <div
+              className="h-full w-full flex-1 flex flex-col items-center justify-center"
+              {...getRootProps()}
+            >
+              <input {...getInputProps()} />
+              {isDragOver ? (
+                <MousePointerSquareDashed className="h-6 w-6 text-zinc-500 mb-2" />
+              ) : isUploading ? (
+                <Loader2 className="animate-spin h-6 w-6 text-zinc-500 mb-2" />
+              ) : (
+                <Image className="h-6 w-6 text-zinc-500 mb-2" />
+              )}
+            </div>
+          )}
+        </DropZone>
+      </div>
+    </div>
+  );
 };
 
 export default Page;
